049c5bc3bd49e2a1eb72229a6dc7e1a3f3521ecb
ahmedhassan1010 <ah974578@gmail.com>
2023-03-17 11:09:50 -0700

    exceptionsq_e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Use `putchar` function to print the alphabet in lowercase.
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
