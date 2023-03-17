fa7c5e3c3932917a145f579ff415fa728bbabb74
ahmedhassan1010 <ah974578@gmail.com>
2023-03-17 11:03:14 -0700

    all alphabetaA 2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,17 +11,17 @@
 
 int main(void)
 {
-        char c;
+	char c;
 
-        for (c = 'a'; c <= 'z'; c++)
-        {
-                putchar(c);
-        }
-        for (c = 'A'; c <= 'Z'; c++)
-        {
-                putchar(c);
-        }
-        putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
 
-        return (0);
+	return (0);
 }
