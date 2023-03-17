6a9ab49980eaf5566596079622cf6cd01018e7c4
ahmedhassan1010 <ah974578@gmail.com>
2023-03-17 10:31:23 -0700

    2.printing with putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print statements based on the last digit of the random number.
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar("\n");
+	return (0);
+}
