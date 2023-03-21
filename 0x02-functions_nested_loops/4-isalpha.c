b516c89708b34ab3081e85cd9b684013a6166465
ahmedhassan1010 <ah974578@gmail.com>
2023-03-21 10:50:20 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase
+ * @c: the character to be checked
+ *
+ * Description: This function checks if the input character is an alpha
+ * It uses a conditions to compare the input character with condition
+ * the alphabet. If the character is an alphabet, the function returns 1.
+ * if is not, the function returns 0.
+ *
+ * Return: 1 if the character is lowercase, 0 otherwise.
+ */
+int _islower(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
