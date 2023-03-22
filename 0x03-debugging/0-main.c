f44d9dadd86c987dc90302fc14beeda7c3cbe9e9
ahmedhassan1010 <ah974578@gmail.com>
2023-03-22 06:07:28 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main: is the entry point and test the function if the number is p or n
+ * Return: return 0 always
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
