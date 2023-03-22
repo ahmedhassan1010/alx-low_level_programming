fa4250d6f784e760baedd0d81d354155bcb63747
ahmedhassan1010 <ah974578@gmail.com>
2023-03-22 06:18:46 -0700

    multiple mains 3

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * main: test the function if the number is p or n
- * Return: return 0 always
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
 
 int main(void)
